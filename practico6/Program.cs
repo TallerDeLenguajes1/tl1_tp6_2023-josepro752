0b7fda39d179aa270ea4721f4d8bd8642cc379e2
josepro752 <joseignacioboggio@gmail.com>
2023-05-18 19:32:56 -0300

    termino el punto 2

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -26,3 +26,56 @@
 
 /////// Ejercicio 2 ///////
 
+bool resp = true, resultado;
+int operacion = 1, opcion, num1, num2;
+Console.WriteLine("------CalculadoraV1-------");
+while (resp) {
+    Console.WriteLine("---Operacion N"+operacion+"---");
+    Console.WriteLine("Seleccione la operacion que desea realizar");
+    Console.WriteLine("1_Suma");
+    Console.WriteLine("2_Resta");
+    Console.WriteLine("3_Mulitplicacion");
+    Console.WriteLine("4_Division");
+    resultado = int.TryParse(Console.ReadLine(), out opcion);
+    if (resultado) {
+        if (opcion > 0 && opcion < 5) {
+            Console.WriteLine("Escriba el primer numero a operar");
+            resultado = int.TryParse(Console.ReadLine(), out num1);
+            if (resultado) {
+                Console.WriteLine("Escriba el segundo numero a operar");
+                resultado = int.TryParse(Console.ReadLine(), out num2);
+                if (resultado) {
+                    switch(opcion) {
+                        case 1:
+                            num1 = num1 + num2;
+                        break;
+                        case 2:
+                            num1 = num1 - num2;
+                        break;
+                        case 3:
+                            num1 = num1 * num2;
+                        break;
+                        case 4:
+                            num1 = num1/num2;
+                        break;
+                    }
+                    Console.WriteLine("Resultado: "+num1);
+                } else {
+                    Console.WriteLine("No se ingresaron numeros");
+                }
+            } else {
+                Console.WriteLine("No se ingresaron numeros");
+            }
+        }
+    }  else {
+        Console.WriteLine("Numero de operacion ingresado no valido");
+    }
+    Console.WriteLine("Desea realizar otra operacion (SI:1/NO:0)");
+    resultado = int.TryParse(Console.ReadLine(), out num1);
+    if (resultado) {
+        if (num1 == 0) {
+            resp = false;
+        }
+    }
+    operacion++;
+}
