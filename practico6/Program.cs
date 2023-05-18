603971d9546ddfcd592e7a4f471a57953e53fe9a
josepro752 <joseignacioboggio@gmail.com>
2023-05-18 18:48:10 -0300

    termine el punto 1

---
+++ practico6/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// int a;
+// int b;
+// a=10;
+// b=a;
+// Console.WriteLine("valor de a:"+a);
+// Console.WriteLine("valor de b:"+b);
+
+/////// Ejercicio 1 ///////
+
+// string num;
+// int realNum, aux = 0;
+// bool resultado;
+
+// Console.WriteLine("Escriba un numero");
+// num = Console.ReadLine();
+// resultado = int.TryParse(num, out realNum);
+// if (resultado) {
+//     if (realNum > 0) {
+//         while (realNum != 0) {
+//             aux = (aux * 10) + realNum%10;
+//             realNum = realNum/10;
+//         }
+//         Console.WriteLine("El numero invertido es: "+aux);
+//     }
+// }
+
+/////// Ejercicio 2 ///////
+
