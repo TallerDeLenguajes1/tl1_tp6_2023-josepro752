c77cc133eb8363a8a3be1617e4b74de4448d4658
josepro752 <joseignacioboggio@gmail.com>
2023-05-23 17:09:26 -0300

    termino el punto 3 sin hacer merge

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -26,56 +26,94 @@
 
 /////// Ejercicio 2 ///////
 
+// bool resp = true, resultado;
+// int operacion = 1, opcion, num1, num2;
+// Console.WriteLine("------CalculadoraV1-------");
+// while (resp) {
+//     Console.WriteLine("---Operacion N"+operacion+"---");
+//     Console.WriteLine("Seleccione la operacion que desea realizar");
+//     Console.WriteLine("1_Suma");
+//     Console.WriteLine("2_Resta");
+//     Console.WriteLine("3_Mulitplicacion");
+//     Console.WriteLine("4_Division");
+//     resultado = int.TryParse(Console.ReadLine(), out opcion);
+//     if (resultado) {
+//         if (opcion > 0 && opcion < 5) {
+//             Console.WriteLine("Escriba el primer numero a operar");
+//             resultado = int.TryParse(Console.ReadLine(), out num1);
+//             if (resultado) {
+//                 Console.WriteLine("Escriba el segundo numero a operar");
+//                 resultado = int.TryParse(Console.ReadLine(), out num2);
+//                 if (resultado) {
+//                     switch(opcion) {
+//                         case 1:
+//                             num1 = num1 + num2;
+//                         break;
+//                         case 2:
+//                             num1 = num1 - num2;
+//                         break;
+//                         case 3:
+//                             num1 = num1 * num2;
+//                         break;
+//                         case 4:
+//                             num1 = num1/num2;
+//                         break;
+//                     }
+//                     Console.WriteLine("Resultado: "+num1);
+//                 } else {
+//                     Console.WriteLine("No se ingresaron numeros");
+//                 }
+//             } else {
+//                 Console.WriteLine("No se ingresaron numeros");
+//             }
+//         }
+//     }  else {
+//         Console.WriteLine("Numero de operacion ingresado no valido");
+//     }
+//     Console.WriteLine("Desea realizar otra operacion (SI:1/NO:0)");
+//     resultado = int.TryParse(Console.ReadLine(), out num1);
+//     if (resultado) {
+//         if (num1 == 0) {
+//             resp = false;
+//         }
+//     }
+//     operacion++;
+// }
+
+/////// Ejercicio 3 ///////
+
 bool resp = true, resultado;
-int operacion = 1, opcion, num1, num2;
-Console.WriteLine("------CalculadoraV1-------");
-while (resp) {
-    Console.WriteLine("---Operacion N"+operacion+"---");
-    Console.WriteLine("Seleccione la operacion que desea realizar");
-    Console.WriteLine("1_Suma");
-    Console.WriteLine("2_Resta");
-    Console.WriteLine("3_Mulitplicacion");
-    Console.WriteLine("4_Division");
-    resultado = int.TryParse(Console.ReadLine(), out opcion);
+int operacion = 1, opcion;
+float num, num2;
+Console.WriteLine("------CalculadoraV2-------");
+Console.WriteLine("Escriba un numero");
+resultado = float.TryParse(Console.ReadLine(), out num);
+if (resultado) {
+    Console.WriteLine("El valor absoluto del numero es: "+Math.Abs(num));
+    Console.WriteLine("El cuadrado del numero es: "+num*num);
+    Console.WriteLine("La raiz cuadrada del numero es: "+Math.Sqrt(num));
+    Console.WriteLine("El seno del numero es: "+Math.Sin(num));
+    Console.WriteLine("El coseno del numero es: "+Math.Cos(num));
+    Console.WriteLine("La parte entera del numero es: "+Math.Truncate(num));
+} else {
+    Console.WriteLine("No se ingresaron numeros");
+}
+Console.WriteLine("Escriba un numero");
+resultado = float.TryParse(Console.ReadLine(), out num);
+if (resultado) {
+    Console.WriteLine("Escriba otro numero");
+    resultado = float.TryParse(Console.ReadLine(), out num2);
     if (resultado) {
-        if (opcion > 0 && opcion < 5) {
-            Console.WriteLine("Escriba el primer numero a operar");
-            resultado = int.TryParse(Console.ReadLine(), out num1);
-            if (resultado) {
-                Console.WriteLine("Escriba el segundo numero a operar");
-                resultado = int.TryParse(Console.ReadLine(), out num2);
-                if (resultado) {
-                    switch(opcion) {
-                        case 1:
-                            num1 = num1 + num2;
-                        break;
-                        case 2:
-                            num1 = num1 - num2;
-                        break;
-                        case 3:
-                            num1 = num1 * num2;
-                        break;
-                        case 4:
-                            num1 = num1/num2;
-                        break;
-                    }
-                    Console.WriteLine("Resultado: "+num1);
-                } else {
-                    Console.WriteLine("No se ingresaron numeros");
-                }
-            } else {
-                Console.WriteLine("No se ingresaron numeros");
-            }
+        if (num < num2) {
+            Console.WriteLine("El mayor numero es: "+num2);
+            Console.WriteLine("El menor numero es: "+num);
+        } else {
+            Console.WriteLine("El mayor numero es: "+num);
+            Console.WriteLine("El menor numero es: "+num2);        
         }
-    }  else {
-        Console.WriteLine("Numero de operacion ingresado no valido");
+    } else {
+        Console.WriteLine("No se ingresaron numeros");
     }
-    Console.WriteLine("Desea realizar otra operacion (SI:1/NO:0)");
-    resultado = int.TryParse(Console.ReadLine(), out num1);
-    if (resultado) {
-        if (num1 == 0) {
-            resp = false;
-        }
-    }
-    operacion++;
+} else {
+    Console.WriteLine("No se ingresaron numeros");
 }
